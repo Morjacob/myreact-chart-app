112c40d7ca5b4ef91f4d4334488910a183030e86
Morgan Jacob <jacobmmori@gmail.com>
2024-12-08 17:35:48 -0500

    fixed error

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 
 import React, { useState, useEffect } from 'react';
-import './App.css';
 import BarChart from './components/BarChart';
 import LineChart from './components/LineChart';
 import ScatterChart from './components/ScatterChart';
